0261771ed38179bbb86789f2b194afc36ff9037a
yuriy.kopytov <yuriy.kopytov@ab-soft.net>
2022-10-18 12:41:15 +0200

    New test method testThatTooYongUsersCanNotPlay was added

--- src/test/java/net/absoft/mortalWombat/AgeCheckerTest.java
+++ src/test/java/net/absoft/mortalWombat/AgeCheckerTest.java
@@ -10,4 +10,11 @@ public class AgeCheckerTest {
         AgeChecker ageChecker = new AgeChecker();
         Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can't play game");
     }
+
+    @Test
+    public void testThatTooYongUsersCanNotPlay () {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.canPlayGame(15), "User's age is too low to play the game");
+    }
+
 }
