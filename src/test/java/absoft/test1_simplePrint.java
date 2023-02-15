252ca8d5a00c7113b65199a2608c884aea496543
yuriy.kopytov <yuriy.kopytov@ab-soft.net>
2023-02-15 23:10:33 +0100

    Tests for education are added

---
+++ src/test/java/absoft/test1_simplePrint.java
@@ -0,0 +1,19 @@
+package absoft;
+// import java.util.Scanner;
+
+/*
+Напишите программу в которой пользователь должен ввести с клавиатуры name и age
+
+Выведите на экран 2 строки:
+Hello (name)
+Your age is (age)
+ */
+
+public class test1_simplePrint {
+
+    public static void main (String [] args) {
+
+    }
+}
+
+
